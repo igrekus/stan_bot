b203e200b356f7793be43b1462dfe4b517804d22
igrekus <upload234@mail.ru>
2020-04-13 19:22:24 +0300

    WIP: add proxy

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 import json
+import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
 
 with open('creds.json', 'rt', encoding='utf-8') as f:
-    settings = json.loads(''.join(f.readlines()))
+    creds = json.loads(''.join(f.readlines()))
 
-bot = Bot(token=settings['token'])
+px_user = creds['user']
+px_pass = creds['pass']
+px_server = creds['proxy']
+px_port = creds['port']
+token = creds['token']
+proxy = f'socks5://{px_user}:{px_pass}@{px_server}:{px_port}'
+
+bot = Bot(token=token, proxy=proxy)
 dp = Dispatcher(bot)
+
+logging.basicConfig(level=logging.INFO)
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, 'test message')
-    # if message.text == 'start':
-    #     await message.reply('start command issued')
-    # elif message.text == 'help':
-    #     await message.reply('help command issued')
+    if message.text == 'start':
+        await message.reply('start command issued')
+    elif message.text == 'help':
+        await message.reply('help command issued')
 
 
 @dp.message_handler(regexp='(^cat[s]?$|puss)')
 async def cats(message: types.Message):
     with open('data/cats.jpg', 'rb') as photo:
-        '''
-        # Old fashioned way:
-        await bot.send_photo(
-            message.chat.id,
-            photo,
-            caption='Cats are here 😺',
-            reply_to_message_id=message.message_id,
-        )
-        '''
         await message.reply_photo(photo, caption='катан!!1 😺')
 
 
