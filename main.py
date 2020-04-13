4090c79aa89553793770b12e03b6efe3d48986c9
igrekus <upload234@mail.ru>
2020-04-13 19:42:42 +0300

    WIP: add inline message example

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import hashlib
 import json
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-
+from aiogram.types import InlineQueryResultArticle, InputTextMessageContent, InlineQuery
 
 with open('creds.json', 'rt', encoding='utf-8') as f:
     creds = json.loads(''.join(f.readlines()))
@@ -34,9 +35,18 @@ async def cats(message: types.Message):
         await message.reply_photo(photo, caption='катан!!1 😺')
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.inline_handler()
+async def inline_echo(inline_query: InlineQuery):
+    text = inline_query.query or 'echo'
+    input_content = InputTextMessageContent(text)
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    item = InlineQueryResultArticle(
+        id=result_id,
+        title=f'Result {text!r}',
+        input_message_content=input_content,
+    )
+    # don't forget to set cache_time=1 for testing (default is 300s or 5m)
+    await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
 
 
 def main():
