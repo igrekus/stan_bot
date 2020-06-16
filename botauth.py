7f4f83c8e99c2f94f08114f221e673c02b59e1e7
igrekus <upload234@mail.ru>
2020-06-16 17:24:38 +0300

    FEAT: implement voice/devoice bang commands

--- botauth.py
+++ botauth.py
@@ -22,12 +22,42 @@ class BotAuth:
         self._add_base_permits(new_bot_user)
         return True
 
-    def has_permission(self, bot_user):
+    def has_base_permits(self, bot_user):
         if not list(self.authorized.find(tg_id=bot_user.id)):
             return False
         return bool(list(
             self.user_permit_map.find(tg_user=bot_user.id, tg_permit=[perm['id'] for perm in self.base_permits])
         ))
+
+    def voice(self, tg_user):
+        if not self.bot_user_exists(tg_user):
+            return False
+        return self._voice_bot_user({
+            'tg_id': tg_user.id,
+            'username': tg_user.username,
+            'first_name': tg_user.first_name,
+            'last_name': tg_user.last_name,
+        })
+
+    def devoice(self, tg_user):
+        if not self.bot_user_exists(tg_user):
+            return False
+        return self._devoice_bot_user({
+            'tg_id': tg_user.id,
+            'username': tg_user.username,
+            'first_name': tg_user.first_name,
+            'last_name': tg_user.last_name,
+        })
+
+    def _voice_bot_user(self, bot_user):
+        # TODO error handling
+        self._add_base_permits(bot_user)
+        return True
+
+    def _devoice_bot_user(self, bot_user):
+        # TODO error handling
+        self._revoke_base_permits(bot_user)
+        return True
 
     def _upsert_bot_user(self, user):
         new_bot_user = {
@@ -45,3 +75,7 @@ class BotAuth:
                 'tg_user': new_bot_user['tg_id'],
                 'tg_permit': perm['id']
             }, ['tg_user', 'tg_permit'])
+
+    def _revoke_base_permits(self, bot_user):
+        for perm in self.base_permits:
+            self.user_permit_map.delete(tg_user=bot_user['tg_id'], tg_permit=perm['id'])
